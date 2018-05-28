a641c9fd7f5088585d9c61a45a878f542ed4a206
Michael Bestas <mkbestas@lineageos.org>
2018-05-29 00:33:33 +0300

    Build vr.* with BOARD_VNDK_VERSION
    Add libhardware_headers to the dependency list since
    hardware/libhardware/include is not added to the global include path
    when building with BOARD_VNDK_VERSION.
    
    Test: BOARD_VNDK_VERSION=current m -j vr.msm8996
    Change-Id: I908aed7cf9c5aa970d1e4486dcff32d20d44aebd

--- Android.mk
+++ Android.mk
@@ -32,6 +32,7 @@ LOCAL_SRC_FILES += vr-845.c
 endif
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter
 
